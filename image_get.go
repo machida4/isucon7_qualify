8209ae84808b41a94d1b3e820f8bfe9188aab974
machida <machida@kmc.gr.jp>
2020-08-28 16:54:09 +0900

    image_get.goを作成

---
+++ image_get.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"io/ioutil"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "isucon:isucon@/isubata")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	defer db.Close();
+
+	rows, err := db.Query("SELECT 'name', 'data' FROM image")
+	if err != nil {
+		log.Fatalf(err.Error());
+	}
+	defer rows.Close();
+
+	type Image struct (
+		name string
+		data []byte
+	)
+
+	for rows.Next() {
+		image := Image{}
+
+		err2 := rows.Scan(image.name, image.data)
+		if err != nil {
+			log.Fatalf(err2.Error());
+		}
+
+		fmt.Println(name)
+
+		err3 := ioutil.WriteFile(name, data, 0666)
+		if err != nil {
+			log.Fatalf(err3.Error());
+		}
+	}
+}
